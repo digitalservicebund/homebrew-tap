0054c0f6c4400657bffe1fd59e727a3d8c5ae0b4
Klaus Hartl <klaus.hartl@digitalservice.bund.de>
2023-12-18 16:34:49 +0100

    fix: incomplete (copy&paste) download strategy

--- github_private_repository_release_download_strategy.rb
+++ github_private_repository_release_download_strategy.rb
@@ -1,9 +1,11 @@
 # Downloads tarballs from GitHub releases part of a private repository.
 # Requires a GitHub access token via `HOMEBREW_GITHUB_API_TOKEN` environment
 # variable to authorize the request.
-class GitHubPrivateRepositoryReleaseDownloadStrategy < GitHubPrivateRepositoryDownloadStrategy
+class GitHubPrivateRepositoryReleaseDownloadStrategy < CurlDownloadStrategy
   def initialize(url, name, version, **meta)
     super
+    parse_url_pattern
+    set_github_token
   end
 
   def parse_url_pattern
@@ -15,8 +17,29 @@ class GitHubPrivateRepositoryReleaseDownloadStrategy < GitHubPrivateRepositoryDo
     _, @owner, @repo, @tag, @filename = *@url.match(url_pattern)
   end
 
+  def set_github_token
+    @github_token = ENV["HOMEBREW_GITHUB_API_TOKEN"]
+    unless @github_token
+      raise CurlDownloadStrategyError, "Environmental variable HOMEBREW_GITHUB_API_TOKEN is required."
+    end
+
+    validate_github_repository_access!
+  end
+
   def download_url
     "https://api.github.com/repos/#{@owner}/#{@repo}/releases/assets/#{asset_id}"
+  end
+
+  def validate_github_repository_access!
+    GitHub.repository(@owner, @repo)
+  rescue GitHub::HTTPNotFoundError
+    # We only handle HTTPNotFoundError here,
+    # because AuthenticationFailedError is handled within util/github.
+    message = <<~EOS
+      HOMEBREW_GITHUB_API_TOKEN can not access the repository: #{@owner}/#{@repo}
+      This token may not have permission to access the repository or the url of formula may be incorrect.
+    EOS
+    raise CurlDownloadStrategyError, message
   end
 
   private
